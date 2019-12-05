3e2f17ac6ecee20b7c7174160b8212ffa847f998
asche910 <asche910@163.com>
2019-12-05 11:24:32 +0800

    switch to session instead of jwt

--- src/main/java/com/asche/wetalk/config/SecurityConfig.java
+++ src/main/java/com/asche/wetalk/config/SecurityConfig.java
@@ -15,31 +15,31 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // 关闭csrf验证
-        http.csrf().disable()
-                // 对请求进行认证
-                .authorizeRequests()
-                // 所有 / 的所有请求 都放行
-                .antMatchers("/", "/login").permitAll()
-                // 所有 /login 的POST请求 都放行
-                .antMatchers(HttpMethod.POST, "/loginCheck").permitAll()
-                // 添加权限检测
-                .antMatchers("/fileUpload").hasAuthority("AUTH_WRITE")
-                // 角色检测
-                .antMatchers("/vip").hasRole("ADMIN")
-                // 所有请求需要身份认证
-                .anyRequest().authenticated()
-                .and()
-                // 添加一个过滤器 所有访问 /login 的请求交给 JWTLoginFilter 来处理 这个类处理所有的JWT相关内容
-                // TODO  进一步了解Authentication
-//                .addFilterBefore(new JWTLoginFilter("/loginCheck", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
-                // 添加一个过滤器验证其他请求的Token是否合法
-                .addFilterBefore(new JWTAuthenticationFilter(),
-                        UsernamePasswordAuthenticationFilter.class);
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        // 关闭csrf验证
+//        http.csrf().disable()
+//                // 对请求进行认证
+//                .authorizeRequests()
+//                // 所有 / 的所有请求 都放行
+//                .antMatchers("/", "/login").permitAll()
+//                // 所有 /login 的POST请求 都放行
+//                .antMatchers(HttpMethod.POST, "/loginCheck").permitAll()
+//                // 添加权限检测
+//                .antMatchers("/fileUpload").hasAuthority("AUTH_WRITE")
+//                // 角色检测
+//                .antMatchers("/vip").hasRole("ADMIN")
+//                // 所有请求需要身份认证
+//                .anyRequest().authenticated();
+//    /*            .and()
+//                // 添加一个过滤器验证其他请求的Token是否合法
+//                .addFilterBefore(new JWTAuthenticationFilter(),
+//                        UsernamePasswordAuthenticationFilter.class);*/
+//    }
 
+    /**
+     * this function will invoked if the function above is commented!
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // 使用自定义身份验证组件
